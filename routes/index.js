39f31055a96910596bbcee33e03e7c9eae640f13
Jeremy <jeremy.jacobskind@gmail.com>
2014-10-02 15:32:31 -0400

    Update to send to Martin

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,36 @@ var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+	// console.log("AAAAAAAAAAAAAA");
+	// models.Page.distinct('title', function(err, titles) {
+	// 	console.log(titles);
+	// 	res.render('index', { title: 'WikiStack', headline:"BROWSE MY WIKISTACK" });
+	// });
 	models.Page.find({}, function (err, all_pages) {
-		res.render('index', { title: 'WikiStack', docs:all_pages, headline:"BROWSE MY WIKISTACK" });
+		var checkObj = {};
+		var tester = {};
+		if (tester["on"]===undefined)	tester["on"] = "asdf";
+		tester.two = "asdfg";
+		console.log(tester);
+		var objArr = [];
+		all_pages.forEach(function(item) {
+			if(checkObj[item.title]===undefined) {
+				checkObj[item.title]=item;
+			}
+			else {
+				checkObj[item.title].multiple=true;
+				// console.log(checkObj[item.title] + "*********");
+			}
+		});
+		console.log(checkObj);
+		for(var obj in checkObj) {
+			// console.log(obj);
+			if(obj.hasOwnProperty("multiple")) {
+				obj.url_name = "disambiguation/" + obj.url_name;
+			}
+			objArr.push(obj);
+		}
+		res.render('index', { title: 'WikiStack', docs:objArr, headline:"BROWSE MY WIKISTACK" });
 	});
 });
 
--- routes/wiki.js
+++ routes/wiki.js
@@ -9,4 +9,23 @@ router.get('/:url_name', function(req, res) {
 	});
 });
 
+router.post('/pageAction', function(req, res) {
+	var url_name=req.body.url_name;
+	console.log(url_name);
+	var action= req.body.submit;
+	if(action==="Delete") {
+		models.Page.findOne({"url_name":url_name}, function(err, pageObj) {
+			pageObj.remove();
+			res.redirect('/');
+		});
+	}
+	else if(action==="Edit") {
+		models.Page.findOne({"url_name":url_name}, function(err, pageObj) {
+			res.render('add', {title:"WikiStack", headline:"EDIT A PAGE", doc:pageObj});
+		});
+	} else {
+		console.log("Are we getting to the end of the post?");
+	}
+});
+
 module.exports = router;
